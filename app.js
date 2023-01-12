f8ec7f89401b960fd7cb2015bb12e17ebe3f9227
Daksheswar <dakshieswar888@gmail.com>
2023-01-12 16:14:08 +0000

    codingpractice4

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const path = require("path");
+let db = null;
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const dbPath = path.join(__dirname, "cricketTeam.db");
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("server is running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.get("/players/", async (request, response) => {
+  const getQuery = `
+    SELECT * FROM  cricket_team;`;
+  const booksArray = await db.all(getQuery);
+  let myArr = [];
+  const convertDbObjectToResponseObject = (dbObject) => {
+    return {
+      playerId: dbObject.player_id,
+      playerName: dbObject.player_name,
+      jerseyNumber: dbObject.jersey_number,
+      role: dbObject.role,
+    };
+  };
+  for (let each of booksArray) {
+    let a = convertDbObjectToResponseObject(each);
+    myArr.push(a);
+  }
+  response.send(myArr);
+});
+
+//Post
+app.post("/players/", async (request, response) => {
+  const playerDetails = request.body;
+  const { playerId, playerName, jerseyNumber, role } = playerDetails;
+  const addPlayer = `
+    INSERT INTO cricket_team
+    (playerId,playerName,jerseyNumber,role)
+    VALUES
+    ('${playerId}',${playerName},${jerseyNumber},
+    ${role});`;
+  const dpResponse = await db.run(addPlayer);
+  const playId = dpResponse.lastID;
+  response.send(playId);
+  console.log("hi");
+});
